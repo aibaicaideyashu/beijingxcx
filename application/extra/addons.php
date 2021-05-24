62fefe11272806c25fc42b968a81e1294bf78353
longzemin <502827585@qq.com>
2021-05-24 11:00:54 +0800

    初次提交

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -4,6 +4,14 @@ return array (
   'autoload' => false,
   'hooks' => 
   array (
+    'get_cfg' => 
+    array (
+      0 => 'litestore',
+    ),
+    'config_init' => 
+    array (
+      0 => 'nkeditor',
+    ),
   ),
   'route' => 
   array (
